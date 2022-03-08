ff5dca64e126dbe8a3d9a1eec4b3c297d7e09ced
le.zhang <le.zhang@silot.ai>
2022-03-08 17:09:16 +0800

    update READme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This repository including common used function:
    
 # [pypi](https://pypi.org/project/utils-for-ds/)  
 # [github](https://github.com/ZhangLe59151/ds_utils)  
+# [Document](https://zhanglenus.gitbook.io/doc-utils-for-datascientist/)  
+
--- utils_for_ds/data_utils.py
+++ utils_for_ds/data_utils.py
@@ -8,3 +8,17 @@ def create_date_df(startDate = '20200101', endDate = '20210101'):
   date_pd = pd.DataFrame(date_list)
   date_pd.rename(columns={0:'DATE'}, inplace= True)
   return date_pd
+
+def sg_holiday_feature(holiday_df, startDate = '20200101', endDate = '20210101'):
+  date_pd = create_date_df(startDate = startDate, endDate = endDate)
+  holiday_df.rename(columns = {'Date':'DATE'}, inplace = True)
+  df = date_pd.merge(holiday_df, on='DATE', how = 'left')
+  df['Holiday'] = df['Holiday'].fillna('Non-Holiday')
+  df = df[['DATE', 'Holiday']]
+  df_dummy = pd.get_dummies(df, columns = ['Holiday'])
+  return df_dummy
+
+def get_dummy_value(df, dummy_columns):
+  df_dummy = pd.get_dummies(df, columns = dummy_columns)
+  return df_dummy
+
